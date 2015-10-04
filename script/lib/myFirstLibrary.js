6340b65c3588a3da9441339edfb6a9a9aff05330
Anderson Tao <junkdr@gmail.com>
2015-10-04 15:19:05 -0700

    prototyped gameObject

--- script/lib/myFirstLibrary.js
+++ script/lib/myFirstLibrary.js
@@ -1,17 +1,27 @@
-(function(window){
+(function($, window){
 	'use strict';
 	function define_library(){
 		var Library = {};
-		var name = "Timmy";
-		Library.greet = function(){
-			console.log("hello, this is " + name + ".");
+		var el = $('body');
+		Library.addGameObj = function(){
+
+			el.append("<div class=\"gameObj draggable rotatable resizable\">");
+			$('.gameObj').uniqueId();
+			$('.draggable').draggable();
+
+			// $('.resizable').resizable({
+			//     aspectRatio: true,
+			//     handles: 'ne, se, sw, nw'
+			// });	
+			
 		};
 		return Library;
 	}
 
 	if(typeof(Library) === "undefined"){
 		window.Library = define_library();
+		// $.fn.extend(define_library());
 	} else {
 		console.log("Library is alaredy define.");
 	}
-})(window);
+})($, window);
