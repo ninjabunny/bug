3aedf11e60f11e3cb5b17e1cdfd9b69321a5c9da
Anderson Tao <junkdr@gmail.com>
2015-10-06 22:54:43 -0700

    WIP firebase

--- script/lib/myFirstLibrary.js
+++ script/lib/myFirstLibrary.js
@@ -3,7 +3,18 @@
 	function define_library(){
 		var Library = {};
 		var el = $('body');
+		var bodyState;
+		var myFirebaseRef = new Firebase("https://glaring-heat-9883.firebaseio.com/");
+
 		Library.addGameObj = function(){
+
+			el.append("<div class=\"gameObj draggable rotatable resizable\">");
+			$('.gameObj').uniqueId();
+			$('.draggable').draggable();
+
+			el.append("<div class=\"gameObj draggable rotatable resizable\">");
+			$('.gameObj').uniqueId();
+			$('.draggable').draggable();
 
 			el.append("<div class=\"gameObj draggable rotatable resizable\">");
 			$('.gameObj').uniqueId();
@@ -14,6 +25,21 @@
 			//     handles: 'ne, se, sw, nw'
 			// });	
 			
+		};
+		Library.save = function(){
+			bodyState = $('body').clone();
+			var json_text = JSON.stringify(bodyState, null, 2);
+			myFirebaseRef.set(json_text);
+		};
+
+		Library.restore = function(){
+			var tempRestoredState;
+			myFirebaseRef.on("value", function(snapshot) {
+				tempRestoredState = snapshot.val();
+			});
+			var your_object = JSON.parse(tempRestoredState);
+
+			$('body').empty().prepend($(your_object));
 		};
 		return Library;
 	}
